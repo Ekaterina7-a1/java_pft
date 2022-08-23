ef558190755341bf5c7fef6b4e564711ab24243f
Ekaterina7-a1 <78071953+Ekaterina7-a1@users.noreply.github.com>
2022-08-23 15:19:03 +0300

    Выполнен рефакторинг кода

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -16,15 +16,15 @@ public class GroupCreationTests {
     wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     js = (JavascriptExecutor) wd;
     wd.get("http://localhost/addressbook/group.php");
-    login("user", "admin", "pass", "secret", By.xpath("//input[@value='Login']"));
+    login("user", "pass", By.xpath("//input[@value='Login']"), "admin", "secret");
   }
 
-  private void login(String user, String admin, String pass, String secret, By xpath, CharSequence username) {
+  private void login(String user, String pass, By xpath, String username, String password) {
     wd.findElement(By.name(user)).clear();
     wd.findElement(By.name(user)).sendKeys(username);
     wd.findElement(By.name(pass)).click();
     wd.findElement(By.name(pass)).clear();
-    wd.findElement(By.name(pass)).sendKeys(secret);
+    wd.findElement(By.name(pass)).sendKeys(password);
     wd.findElement(xpath).click();
   }
 
@@ -47,12 +47,7 @@ public class GroupCreationTests {
 
   private void fillGroupForm(GroupData groupData) {
     wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    wd.findElement(By.name("group_footer")).click();
+    login("group_name", "group_header", By.name("group_footer"), groupData.getName(), groupData.getHeader());
     wd.findElement(By.name("group_footer")).clear();
     wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
     wd.findElement(By.name("submit")).click();
