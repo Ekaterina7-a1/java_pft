8bff013a48730f5cc1cf28bbb65bded90ad49236
Ekaterina7-a1 <78071953+Ekaterina7-a1@users.noreply.github.com>
2022-08-24 09:48:39 +0300

    Исправлено задание 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,11 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
   public static void main(String[] args) {
-    Pointsdata a = new Pointsdata(1.5, -9.5);
-    Pointsdata b = new Pointsdata(5.5, 1.1);
-    System.out.println("Расстояние между точками = " + distance(a, b));
-  }
-  public static double distance(Pointsdata p1, Pointsdata p2) {
-    return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x)+(p2.y - p1.y)*(p2.y - p1.y));
+    Pointsdata p1 = new Pointsdata(1.5, -9.5);
+    Pointsdata p2 = new Pointsdata(5.5, 1.1);
+    System.out.println("Расстояние между точками = " + p1.distance(p2));
   }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Pointsdata.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Pointsdata.java
@@ -6,4 +6,7 @@ public class Pointsdata {
     this.x = x;
     this.y = y;
   }
+  public double distance(Pointsdata p2) {
+    return Math.sqrt((p2.x - this.x)*(p2.x - this.x)+(p2.y - this.y)*(p2.y - this.y));
+  }
   }
