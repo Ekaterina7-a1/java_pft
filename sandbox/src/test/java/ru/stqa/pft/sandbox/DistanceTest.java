d31dc0ea9d25d444aae56678481e42672fc228e2
Ekaterina7-a1 <78071953+Ekaterina7-a1@users.noreply.github.com>
2022-08-29 09:12:54 +0300

    Испрален тесты

--- sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTest.java
@@ -10,9 +10,10 @@ public class DistanceTest {
     Pointsdata p2 = new Pointsdata(5.5, 1.1);
     Assert.assertEquals(p1.distance(p2), 11.329607230614839);
   }
+  @Test
   public void testDistanceTwo() {
     Pointsdata p1 = new Pointsdata(-5.5, -6.0);
     Pointsdata p2 = new Pointsdata(99.9, 11.11);
-    Assert.assertEquals(p1.distance(p2), 11.329607230614839);
+    Assert.assertEquals(p1.distance(p2), 106.77973637352736);
   }
 }
