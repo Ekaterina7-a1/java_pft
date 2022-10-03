5fe9b155ae45863a2411c8b7dd724b053cfc7584
Ekaterina7-a1 <78071953+Ekaterina7-a1@users.noreply.github.com>
2022-10-03 18:28:25 +0300

    Задание №11: Реализовать тест для проверки информации о контактах на главной странице правка

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactTests.java
@@ -3,7 +3,6 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -11,7 +10,7 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactEmailTests extends TestBase{
+public class ContactTests extends TestBase{
   @BeforeMethod
   public void ensurePreconditions() {
     ContactData contact = new ContactData().
@@ -34,11 +33,35 @@ public class ContactEmailTests extends TestBase{
   private String mergeEmails(ContactData contact) {
     return Arrays.asList(contact.getEmail(),contact.getEmail2(),contact.getEmail3())
             .stream().filter((s)-> !s.equals(""))
-            .map(ContactPhoneTests::cleaned)
+            .map(ContactTests::cleanedemail)
             .collect(Collectors.joining("\n"));
   }
 
-  public static String cleaned (String email) {
+  public static String cleanedemail (String email) {
     return email.replaceAll("\\s","").replaceAll("[-().]]","");
+  }
+  @Test
+  public void testContactPhones(){
+    app.goTo().gotoHomePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+  }
+
+  private String mergePhones(ContactData contact) {
+    return Arrays.asList(contact.getHomePhone(),contact.getMobilePhone(),contact.getWorkPhone())
+            .stream().filter((s)-> !s.equals(""))
+            .map(ContactTests::cleanedphone)
+            .collect(Collectors.joining("\n"));
+  }
+  public static String cleanedphone (String phone) {
+    return phone.replaceAll("\\s","").replaceAll("[-()]]","");
+  }
+  @Test
+  public void testContactAddresses() {
+    app.goTo().gotoHomePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
   }
 }
