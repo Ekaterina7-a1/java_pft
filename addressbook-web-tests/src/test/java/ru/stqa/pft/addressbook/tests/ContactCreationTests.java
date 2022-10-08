aa6edfe2dccd29369099d6741923cb2d66b1a59b
Ekaterina7-a1 <78071953+Ekaterina7-a1@users.noreply.github.com>
2022-10-08 15:16:18 +0300

    Задание №13: Параметризовать тесты для создания групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,25 +1,46 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase{
-  @Test
-  public void testContactCreation() throws Exception {
-    Contacts before = app.contact().all();
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+    }.getType());
+    return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validContactsFromJson")
+  public void testContactCreation(ContactData contactData) throws Exception {
     File photo = new File ("src/test/resources/bob.jpg");
-    ContactData contact = new ContactData().
-    withFirstname("Ekaterina").withLastname("Leonkina").withNickname("leokate")
-            .withAddress("city").withHomePhone("123456789").withMobile("+79997777777").withWorkPhone("9998888888").withPhone2("+7(555)111-55-25").withEmail("test@yandex.ru").
-            withEmail2("ksat@yandex.ru").withEmail3("kaytetest@yandex.ru").withGroup("test1").withPhoto(photo);
-    app.contact().createContact(contact);
+    contactData.withPhoto(photo);
+    Contacts before = app.contact().all();
+    app.contact().createContact(contactData);
     Contacts after = app.contact().all();
     assertThat(after.size(), equalTo(before.size() + 1));
-    assertThat(after,equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+    assertThat(after,equalTo(before.withAdded(contactData.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
   }
 }
